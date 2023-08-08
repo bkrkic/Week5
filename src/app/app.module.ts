bad31b178f0a3901b0ed9d632c57b8576d491547
bkrkic <begokrkic@gmail.com>
2023-08-08 22:02:39 +1000

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
+import { FormsModule } from '@angular/forms';
+// for using [(ngModel)] - Two-way data binding to a html form element
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AccountComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule, // needed for [(ngModel)]
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+//4.3 Angular Routing - import from '@angular/router'
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent {
+
+  //4.3 Angular Routing
+  constructor(private router: Router){};
+
+  //Task 4. Creating Login Credentials
+  email: string = "";
+  password: string = "";
+
+  user_ls = [
+    {username: 'admin', password: 'password'},
+    {username: 'student', password: '123'},
+    {username: 'bego', password: 'krkic'}
+  ]
+
+  error: boolean = true;
+
+
+  signInEvent(){
+  // Event Handler Method - Verifying Login Credentials (From Lecture Slides 4.4)
+    let user = {username: this.email, password: this.password};
+    // If the identity of a user matches a user in the list
+    for (let identity of this.user_ls){
+      if (JSON.stringify(user) === JSON.stringify(identity)){
+        // Move to account page
+        this.router.navigateByUrl('/account');
+        // Set error to false
+        this.error = false;
+        console.log(this.error)
+      }
+    }
+    // Display an error message if true
+    if (this.error == true){
+      alert("Invalid user credentials!");
+    }
+  // End of SignIn Method
+  }
+}
+
+
+
+  // Event Handler Method - Verifying Login Credentials (From Lecture Slides 4.4) - signInEvent(username, password)
+  // signInEvent = (email: string, password: string) => {
+  //   let credentials = this.accounts.find(account => account.username == email && account.password == password)
+  //     if (credentials){
+  //       this.router.navigate(['account'])
+  //     } else {
+  //       alert("Invalid user credentials!")
+  //     }
+  // }
